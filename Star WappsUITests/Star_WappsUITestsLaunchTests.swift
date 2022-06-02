30c1441433e1323b61bcb801ac1d18914e7b29be
Romulo <romulooviganico@gmail.com>
2022-06-02 20:32:11 -0300

    Initial Commit

---
+++ Star WappsUITests/Star_WappsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Star_WappsUITestsLaunchTests.swift
+//  Star WappsUITests
+//
+//  Created by Romulo Oliveira Viganico on 02/06/22.
+//
+
+import XCTest
+
+class Star_WappsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
